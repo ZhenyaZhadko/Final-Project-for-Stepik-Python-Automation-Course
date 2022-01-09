5a9e8c33e73e32d20091ea8b6b43a24638c675fa
ZhenyaZhadko <zhadko.evgenia@gmail.com>
2022-01-09 22:27:38 +0200

    Homework for lesson 4.3.4

---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def alert_product_name_should_be_identical_to_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.MAIN_PRODUCT_NAME)
+        alert_product_name = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME)
+        assert product_name.text == alert_product_name.text, "Product name is not the same"
+
+    def alert_product_price_should_be_identical_to_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.MAIN_PRODUCT_PRICE)
+        alert_product_price = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_PRICE)
+        assert product_price.text == alert_product_price.text, "Product price is not the same"
