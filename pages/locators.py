008b1701b00366ae73f5d9277ca1f16a0cfa335a
ZhenyaZhadko <zhadko.evgenia@gmail.com>
2022-01-11 23:48:13 +0200

    Homework for lesson 4.3.13

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,23 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+    
+class BasketPageLocators():
+    FULL_BASKET_FORMSET = (By.CSS_SELECTOR, "#basket_formset")
+    CONTINUE_SHOPPING_LINK = (By.CSS_SELECTOR, "#content_inner p:nth-child(1) a")
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_ADDRESS = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_KEY = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_REPEAT = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, ".register_form .btn-lg")
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -12,12 +26,3 @@ class ProductPageLocators():
     ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages > .alert-success .alertinner > strong")
     ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alertinner > p > strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert-success")
-
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
-    
-class BasketPageLocators():
-    FULL_BASKET_FORMSET = (By.CSS_SELECTOR, "#basket_formset")
-    CONTINUE_SHOPPING_LINK = (By.CSS_SELECTOR, "#content_inner p:nth-child(1) a")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -16,3 +17,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+
+    def register_new_user(self, email, password):
+        email_address = self.browser.find_element(*LoginPageLocators.EMAIL_ADDRESS)
+        email_address.send_keys(email)
+        password_key = self.browser.find_element(*LoginPageLocators.PASSWORD_KEY)
+        password_key.send_keys(password)
+        password_repeat = self.browser.find_element(*LoginPageLocators.PASSWORD_REPEAT)
+        password_repeat.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+        
